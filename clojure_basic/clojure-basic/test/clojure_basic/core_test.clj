1fb3b3bf5955842c3e47467704e16c50f74aaa90
victorbrandaoa <victor.andrade@ccc.ufcg.edu.br>
2021-06-09 00:17:01 -0300

    add clojure first exercises

---
+++ clojure_basic/clojure-basic/test/clojure_basic/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-basic.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-basic.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
